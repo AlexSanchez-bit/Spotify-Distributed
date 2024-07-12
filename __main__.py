3b4c05d0e2a1af7a4e0d5bdeab30c0b5d01d3c1b
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-07-12 12:10:55 -0400

    test(): add broadcasts in docker files

--- __main__.py
+++ __main__.py
@@ -26,30 +26,9 @@ def main():
     port = int(os.getenv("NODE_PORT", 8080))
     node = KademliaNode(ip, port)
     node.start()
-    for ip, port in default_nodes:
-        print(node.ping(Node(ip, port)))
-    id = f"{time.time()}"
-    time.sleep(0.2)
-    id2 = f"{time.time()}"
-    node.store_playlist(
-        StoreAction.INSERT, Playlist("ocaso de la lluvia vieja", "yo", id)
-    )
-    node.store_playlist(
-        StoreAction.INSERT, Playlist("la nene mas linda del mundo", "yo", id2)
-    )
-    node.store_playlist(
-        StoreAction.DELETE,
-        Playlist("", "", id),
-    )
-    node.get_playlist(id2)
-
-    node.store_a_file("audio.mp3")
-    node.get_a_file(sha1_hash("audio.mp3"))
 
     while True:
-        file_direction = input("ip: ")
-        node.store_a_file(file_direction)
-        time.sleep(1)
+        time.sleep(0.5)
 
 
 if __name__ == "__main__":
